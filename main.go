43593cb341f1f4356b9e7a3eb4cb027298241860
gempurbayu <aji123zz@gmail.com>
2022-08-01 02:10:13 +0700

    GET method & handler

--- main.go
+++ main.go
@@ -16,5 +16,32 @@ func main() {
 		})
 	})
 
+	router.GET("/hello", helloHandler)
+	router.GET("/books/:id/:title", booksHandler)
+	router.GET("/query", queryHandler)
+
+	//custom port :8888
 	router.Run()
+}
+
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"title":  "Hello Guys!",
+		"number": 2,
+	})
+}
+
+func booksHandler(c *gin.Context) {
+	id := c.Param("id")
+	title := c.Param("title")
+
+	c.JSON(http.StatusOK, gin.H{"id": id, "title": title})
+}
+
+// localhost:8080/query?title=[title]&name=[name]
+func queryHandler(c *gin.Context) {
+	title := c.Query("title")
+	name := c.Query("name")
+
+	c.JSON(http.StatusOK, gin.H{"title": title, "name": name})
 }
